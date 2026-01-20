0398addab378491f30287e50a3881e9b3cbbc6d8
Tylka <mateusz.tylka@domdata.com>
2026-01-20 12:47:19 +0100

    mcp server dotnet asp ver

--- McpApp/Program.cs
+++ McpApp/Program.cs
@@ -1,63 +1,40 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ModelContextProtocol.Server;
-using System.ComponentModel;
-using System.Text.Json;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Logging.AddConsole(consoleLogOptions =>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddJsonConsole();
+builder.Logging.AddFilter("ModelContextProtocol", LogLevel.Trace);
+
+builder.Services.AddCors(options =>
 {
-    // Configure all logs to go to stderr
-    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
 
 builder.Services
     .AddMcpServer()
-    .WithStdioServerTransport()
-    .WithToolsFromAssembly(typeof(DynamicToolsMcpApi).Assembly);
+    .WithHttpTransport()
+    .WithToolsFromAssembly(typeof(TestTools).Assembly);
 
-await builder.Build().RunAsync();
+Console.WriteLine("typeof(TestTools).Assembly.FullName");
 
-// --- 
+Console.WriteLine(typeof(TestTools).Assembly.FullName);
+Console.WriteLine("typeof(Program).Assembly.FullName");
 
-// using Microsoft.AspNetCore.Builder;
-// using Microsoft.Extensions.DependencyInjection;
-// using Microsoft.Extensions.Logging;
-// using ModelContextProtocol.Server;
+Console.WriteLine(typeof(Program).Assembly.FullName);
 
-// var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddFilter("ModelContextProtocol.Server.ToolRegistry", LogLevel.Trace);
 
-// builder.Logging.AddJsonConsole();
-// builder.Logging.AddFilter("ModelContextProtocol", LogLevel.Trace);
+var app = builder.Build();
 
-// builder.Services.AddCors(options =>
-// {
-//     options.AddDefaultPolicy(policy =>
-//     {
-//         policy.AllowAnyOrigin()
-//               .AllowAnyHeader()
-//               .AllowAnyMethod();
-//     });
-// });
+app.UseCors();
+app.MapMcp();
 
-// builder.Services
-//     .AddMcpServer()
-//     .WithHttpTransport()
-//     .WithToolsFromAssembly(typeof(TestTools).Assembly);
-
-// Console.WriteLine("typeof(TestTools).Assembly.FullName");
-
-// Console.WriteLine(typeof(TestTools).Assembly.FullName);
-// Console.WriteLine("typeof(Program).Assembly.FullName");
-
-// Console.WriteLine(typeof(Program).Assembly.FullName);
-
-// builder.Logging.AddFilter("ModelContextProtocol.Server.ToolRegistry", LogLevel.Trace);
-
-// var app = builder.Build();
-
-// app.UseCors();
-// app.MapMcp();
-
-// app.Run("http://localhost:3333");
+app.Run("http://localhost:3333");
